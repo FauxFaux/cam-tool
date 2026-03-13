e7ab572fa3c8b5f96184211a8ce3e95b77fb6ece
Chris West (Faux) <git@goeswhere.com>
2026-03-13 19:02:15 +0000

    fix: still overdeleting. try harder to underdelete

--- src/main.rs
+++ src/main.rs
@@ -48,25 +48,41 @@ fn main() -> Result<()> {
         } => {
             let directory = directory.canonicalize()?;
             let use_percentage = read_use_percentage(&directory)?;
-            info!("current: {use_percentage}%, target: {target_use_percentage}%");
+            let bytes_to_free = compute_bytes_to_free(&directory, target_use_percentage)?;
+            info!(
+                "current: {use_percentage}%, target: {target_use_percentage}%, need to free: {:.1}MB",
+                mb(bytes_to_free),
+            );
             if use_percentage < target_use_percentage {
                 return Ok(());
             }
             let mut matches = find_matching_files(&directory, filter_extensions)?;
-            while let Some(candidate) = matches.pop_front() {
+            let mut freed_bytes_estimate: u64 = 0;
+            while let Some((candidate, file_size)) = matches.pop_front() {
                 let use_percentage = read_use_percentage(&directory)?;
-                if use_percentage < target_use_percentage {
+                if use_percentage < target_use_percentage || freed_bytes_estimate >= bytes_to_free {
                     break;
                 }
-                info!("should remove: {:?}", candidate);
+                info!(
+                    "should remove: {:?} ({:.1} MB)",
+                    candidate,
+                    (file_size as f64) / 1024. / 1024.
+                );
                 if actually_rm {
                     fs::remove_file(&candidate)?;
                 }
+                freed_bytes_estimate += file_size;
+
                 sync_all_the_way_down(&candidate)?;
             }
+            info!("freed around {:.1}MB", mb(freed_bytes_estimate));
             Ok(())
         }
     }
+}
+
+fn mb(v: u64) -> f64 {
+    (v as f64) / 1024. / 1024.
 }
 
 fn sync_all_the_way_down(starting_file: impl AsRef<Path>) -> Result<()> {
@@ -81,17 +97,17 @@ fn sync_all_the_way_down(starting_file: impl AsRef<Path>) -> Result<()> {
 fn find_matching_files(
     directory: impl AsRef<Path>,
     filter_extensions: Vec<OsString>,
-) -> Result<VecDeque<PathBuf>> {
+) -> Result<VecDeque<(PathBuf, u64)>> {
     let mut matches = Vec::with_capacity(1024);
     for entry in walkdir::WalkDir::new(directory).into_iter() {
         match dir_entry_to_modified(entry) {
-            Ok(Some((path, modified))) => {
+            Ok(Some((path, modified, size))) => {
                 let ext = match path.extension() {
                     Some(ext) => ext,
                     None => continue,
                 };
                 if filter_extensions.iter().any(|e| e == ext) {
-                    matches.push((path, modified));
+                    matches.push((path, modified, size));
                 }
             }
             Ok(None) => continue,
@@ -101,34 +117,53 @@ fn find_matching_files(
             }
         }
     }
-    matches.sort_unstable_by_key(|(_, modified)| *modified);
-    Ok(matches.into_iter().map(|(path, _)| path).collect())
+    matches.sort_unstable_by_key(|(_, modified, _)| *modified);
+    Ok(matches
+        .into_iter()
+        .map(|(path, _, size)| (path, size))
+        .collect())
 }
 
 fn read_use_percentage(directory: impl AsRef<Path>) -> Result<u8> {
     let stat = nix::sys::statvfs::statvfs(directory.as_ref())?;
-    let use_percentage = u8::try_from(100u64 - (stat.blocks_free() * 100 / (stat.blocks())))?;
+    let total_blocks = stat.blocks();
+    let free_blocks = stat.blocks_free();
+
+    let use_percentage = u8::try_from(100u64 - (free_blocks * 100 / total_blocks))?;
     Ok(use_percentage)
+}
+
+fn compute_bytes_to_free(directory: impl AsRef<Path>, target_use_percentage: u8) -> Result<u64> {
+    let stat = nix::sys::statvfs::statvfs(directory.as_ref())?;
+    let total_blocks = stat.blocks();
+    let free_blocks = stat.blocks_free();
+    let used_blocks = total_blocks - free_blocks;
+    let target_used_blocks = total_blocks * u64::from(target_use_percentage) / 100;
+
+    if used_blocks <= target_used_blocks {
+        return Ok(0);
+    }
+
+    let block_size = stat.fragment_size();
+    Ok((used_blocks - target_used_blocks) * block_size)
 }
 
 fn dir_entry_to_modified(
     entry: walkdir::Result<walkdir::DirEntry>,
-) -> Result<Option<(PathBuf, u64)>> {
+) -> Result<Option<(PathBuf, u64, u64)>> {
     let entry = entry?;
     if !entry.file_type().is_file() {
         return Ok(None);
     }
 
     let path = entry.path();
-
-    let modified = get_file_modified(&entry).with_context(|| anyhow!("reading {:?}", &path))?;
-    Ok(Some((path.to_path_buf(), modified)))
-}
-
-fn get_file_modified(entry: &walkdir::DirEntry) -> Result<u64> {
-    Ok(entry
-        .metadata()?
+    let metadata = entry
+        .metadata()
+        .with_context(|| anyhow!("reading {:?}", &path))?;
+    let modified = metadata
         .modified()?
         .duration_since(std::time::UNIX_EPOCH)?
-        .as_secs())
+        .as_secs();
+    let size = metadata.len();
+    Ok(Some((path.to_path_buf(), modified, size)))
 }
